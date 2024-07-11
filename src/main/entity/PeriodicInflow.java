8a2e46e231ee24c32dda07bb6c769ea9c5c049b9
chifonghuang <chifong.huang@mail.utoronto.ca>
2024-07-11 19:25:24 -0400

    The first version of PeriodicInflow

---
+++ src/main/entity/PeriodicInflow.java
@@ -0,0 +1,25 @@
+package main.entity;
+
+public class PeriodicInflow extends PeriodicTransaction {
+    private String incomeSource;
+
+    public PeriodicInflow(float transactionAmount, String transactionDate, String transactionDescription, String recurrence, String incomeSource) {
+        super(transactionAmount, transactionDate, transactionDescription, recurrence);
+        this.incomeSource = incomeSource;
+    }
+
+    public String getIncomeSource() {
+        return incomeSource;
+    }
+
+    public void setIncomeSource(String incomeSource) {
+        this.incomeSource = incomeSource;
+    }
+
+    @Override
+    public void recordTransaction() {
+        System.out.println("Recording Income: ");
+        displayTransactionDetails();
+        System.out.println("Source: " + incomeSource);
+    }
+}
