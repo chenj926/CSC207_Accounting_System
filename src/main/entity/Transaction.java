f465464021027790300f21edb09c816d984c9b34
chenj926 <shendicjl@gmail.com>
2024-07-11 20:12:52 -0400

    deleted the extra abstract class

--- src/main/entity/Transaction.java
+++ src/main/entity/Transaction.java
@@ -3,13 +3,3 @@ package main.entity;
 public interface Transaction {
     void RecordTransaction(float amount);
 }
-
-
-
-public abstract class OneTimeTransaction implements Transaction {
-    protected float transactionAmount;
-
-    // Abstract method to record the transaction
-    public abstract void recordTransaction(float amount);
-}
-}
