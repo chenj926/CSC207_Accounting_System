9171d5f323d402e5e798490bccbb9ae215f4caab
chenj926 <shendicjl@gmail.com>
2024-07-13 18:15:29 -0400

    fix it to interface

--- src/main/use_case/SignupOutputBoundary.java
+++ src/main/use_case/SignupOutputBoundary.java
@@ -2,7 +2,7 @@ package use_case;
 
 import java.util.*;
 
-public class SignupOutputBoundary {
+public interface SignupOutputBoundary {
     void prepareSuccessView(SignupOutputData user);
 
     void prepareFailView(String error);
