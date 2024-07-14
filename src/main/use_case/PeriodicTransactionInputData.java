b8845089f24e8ff8063a505cc7e8f62e0b0214fd
dana <danak.huang@mail.utoronto.ca>
2024-07-14 18:21:15 -0400

    identification added

--- src/main/use_case/PeriodicTransactionInputData.java
+++ src/main/use_case/PeriodicTransactionInputData.java
@@ -3,16 +3,22 @@ package use_case;
 public class PeriodicTransactionInputData {
 
     private float transactionAmount;
+    private final String identification;
     private String transactionDate;
     private String transactionDescription;
     private String recurrence;
 
-    public PeriodicTransactionInputData(float transactionAmount, String transactionDate, String transactionDescription, String recurrence) {
+    public PeriodicTransactionInputData(String identification, float transactionAmount, String transactionDate, String transactionDescription, String recurrence) {
         this.transactionAmount = transactionAmount;
         this.transactionDate = transactionDate;
         this.transactionDescription = transactionDescription;
         this.recurrence = recurrence;
+        this.identification = identification;
 
+    }
+
+    public String getIdentification() {
+        return identification;
     }
 
     public float getTransactionAmount() {
