53a930654204ab26a901266de36b3931921c8df5
chenj926 <shendicjl@gmail.com>
2024-07-14 05:28:38 -0400

    create signup interactor

--- src/main/use_case/SignupInteractor.java
+++ src/main/use_case/SignupInteractor.java
@@ -1,22 +1,41 @@
 package use_case;
 
 import java.util.*;
-import java.time.LocalTime;
 
 import data_access.UserSignupDataAccessInterface;
 import entity.UserAccount;
 import entity.AccountFactory;
 
 public class SignupInteractor implements SignupInputBoundary{
-    final
+    final AccountFactory accountFactory;
     final SignupOutputBoundary presenter;
+    final UserSignupDataAccessInterface userDataAccessObject;
 
-    public SignupInteractor() {
+    // Constructor
+    public SignupInteractor(UserSignupDataAccessInterface userSignupDataAccessInterface,
+                            SignupOutputBoundary signupOutputBoundary,
+                            AccountFactory accountFactory) {
+        this.accountFactory = accountFactory;
+        this.userDataAccessObject = userSignupDataAccessInterface;
+        this.presenter = signupOutputBoundary;
 
     }
 
     @Override
-    public void excute() {
+    public void excute(SignupInputData signupInputData) {
+        if (userDataAccessObject.existById(signupInputData.getIdentification())) {
+            // user already exist, return back fail view to presenter
+            presenter.prepareFailView("User already exist!!!");
+        }
+        else {
+            // create new user
+            UserAccount newUser = accountFactory.createUserAccount(signupInputData.getUsername(),
+                    signupInputData.getPassword(), signupInputData.getIdentification());
+            userDataAccessObject.save(newUser);  // save this user
 
+            // prepare output to presenter
+            SignupOutputData signupOutputData = new SignupOutputData(newUser.getUsername(), false);
+            presenter.prepareSuccessView(signupOutputData);
+        }
     }
 }
