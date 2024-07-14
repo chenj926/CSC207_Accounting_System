e7e6fd40e0da0ad5c64fc50120b2a4c5d0856845
chenj926 <shendicjl@gmail.com>
2024-07-14 06:56:21 -0400

    update the share account input to pass in the share acc id and set of personal ids in the same time

--- src/main/use_case/ShareAccountInputData.java
+++ src/main/use_case/ShareAccountInputData.java
@@ -4,14 +4,18 @@ import java.util.*;
 
 public class ShareAccountInputData {
     private final Set<String> sharedUserIdentifications;
+    private final String shareAccountIdentification;
 
     // Constructor
-    public ShareAccountInputData(Set<String> sharedUserIdentifications) {
+    public ShareAccountInputData(String shareAccountIdentification,
+                                 Set<String> sharedUserIdentifications) {
         this.sharedUserIdentifications = sharedUserIdentifications;
+        this.shareAccountIdentification = shareAccountIdentification;
     }
 
     // Getters
     public Set<String> getSharedUserIdentifications() {
         return sharedUserIdentifications;
     }
+    public String getShareAccountIdentification() { return shareAccountIdentification; }
 }
