f0bfb7fdea44ef0db6919f50255e799b772937a2
chenj926 <shendicjl@gmail.com>
2024-07-14 02:56:40 -0400

    create interactor of signup

---
+++ src/main/use_case/SignupInteractor.java
@@ -0,0 +1,22 @@
+package use_case;
+
+import java.util.*;
+import java.time.LocalTime;
+
+import data_access.UserSignupDataAccessInterface;
+import entity.UserAccount;
+import entity.AccountFactory;
+
+public class SignupInteractor implements SignupInputBoundary{
+    final
+    final SignupOutputBoundary presenter;
+
+    public SignupInteractor() {
+
+    }
+
+    @Override
+    public void excute() {
+
+    }
+}
