e28d90e535477ba6edda5cf940982bed27566b29
dana <danak.huang@mail.utoronto.ca>
2024-07-14 16:32:00 -0400

    identification added

--- src/main/use_case/OneTimeTransactionInputData.java
+++ src/main/use_case/OneTimeTransactionInputData.java
@@ -3,18 +3,24 @@ package use_case;
 public class OneTimeTransactionInputData {
 
     private float transactionAmount;
+    private final String identification;
     private String transactionDate;
     private String transactionDescription;
     private String transactionCategory;
     private String transactionNotes;
 
-    public OneTimeTransactionInputData(float transactionAmount, String transactionDate, String transactionDescription,
+    public OneTimeTransactionInputData(float transactionAmount, String identification,String transactionDate, String transactionDescription,
                                        String transactionCategory, String transactionNotes) {
         this.transactionAmount = transactionAmount;
+        this.identification = identification;
         this.transactionDate = transactionDate;
         this.transactionDescription = transactionDescription;
         this.transactionCategory = transactionCategory;
         this.transactionNotes = transactionNotes;
+    }
+
+    public String getIdentification() {
+        return identification;
     }
 
     public float getTransactionAmount() {
