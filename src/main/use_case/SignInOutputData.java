986cd3ab70ee2819d41aa16e6307ca02a09bebe3
dana <danak.huang@mail.utoronto.ca>
2024-07-14 18:57:03 -0400

    Sign in output data added

--- src/main/use_case/SignInOutputData.java
+++ src/main/use_case/SignInOutputData.java
@@ -1,2 +1,21 @@
-package use_case;public class SignInOutputData {
+package use_case;
+
+public class SignInOutputData {
+    private final String username;
+    private final boolean useCaseFailed;
+
+    // Constructor
+    public SignInOutputData(String username, boolean useCaseFailed) {
+        this.username = username;
+        this.useCaseFailed = useCaseFailed;
+    }
+
+    // Getters
+    public String getUsername() {
+        return this.username;
+    }
+
+    public boolean isUseCaseFailed() {
+        return this.useCaseFailed;
+    }
 }
