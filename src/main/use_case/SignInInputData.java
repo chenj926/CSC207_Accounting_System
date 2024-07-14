cf0b6c54c069ac50db2a0b8608e92a2a4c2f42d1
dana <danak.huang@mail.utoronto.ca>
2024-07-14 18:54:37 -0400

    SignInInputData

--- src/main/use_case/SignInInputData.java
+++ src/main/use_case/SignInInputData.java
@@ -1,2 +1,25 @@
-package use_case;public class SignInInputData {
+package use_case;
+
+public class SignInInputData {
+    private final String username;
+    private final String password;
+    private final String identification;
+
+    // Constructor
+    public SignInInputData(String username, String password, String identification){
+        this.username = username;
+        this.password = password;
+        this.identification = identification;
+    }
+
+    // getter
+    public String getUsername() {
+        return this.username;
+    }
+    public String getPassword() {
+        return this.password;
+    }
+    public String getIdentification() {
+        return this.identification;
+    }
 }
