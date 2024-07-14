d8bf7b4affcc7ddc73c28d009e2020c929e48175
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-07-13 22:21:38 -0400

    Updated account factory with getters and setters, segregated from data

--- src/main/entity/AccountFactory.java
+++ src/main/entity/AccountFactory.java
@@ -1,21 +1,37 @@
 package entity;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 public class AccountFactory {
-
-    protected static Map<String, List<String>> allAccounts;
+    private String username;
+    private String password;
+    private String identification;
 
     public AccountFactory(String username, String password, String identification) {
-        if (allAccounts.containsKey(identification)) {
-            System.out.println("The identification '" + identification + "' exists in the system, please choose another identification");
-        } else {
-            List<String> list = new ArrayList<>();
-            list.add(username);
-            list.add(password);
-            allAccounts.put(identification, list);
-        }
+        this.username = username;
+        this.password = password;
+        this.identification = identification;
     }
+
+    // getters
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public String getIdentification() {
+        return identification;
+    }
+
+    // setters
+    public void setIdentification(String identification) {
+        this.identification = identification;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
 }
