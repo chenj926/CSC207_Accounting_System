998fd65a850234d0c7626383305be00e71c91db9
dana <danak.huang@mail.utoronto.ca>
2024-07-13 21:00:42 -0400

    UserAccountDataAccessInterface added

---
+++ src/main/data_access/UserAccountDataAccessInterface.java
@@ -0,0 +1,16 @@
+package data_access;
+
+import entity.UserAccount;
+
+public interface UserAccountDataAccessInterface {
+    boolean existById(String identification);
+
+    void updateId(UserAccount userAccount);
+    //account balance
+    void updateBalance(UserAccount userAccount);
+    //account inflow
+    void updateInflow(UserAccount userAccount);
+    //account outflow
+    void updateOutflow(UserAccount userAccount);
+
+}
