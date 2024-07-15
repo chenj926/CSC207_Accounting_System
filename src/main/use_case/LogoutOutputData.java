0c0e80aa1c486af50be28fc18968adcc0867c112
dana <danak.huang@mail.utoronto.ca>
2024-07-15 03:47:44 -0400

    LogoutOutputData code

--- src/main/use_case/LogoutOutputData.java
+++ src/main/use_case/LogoutOutputData.java
@@ -1,4 +1,19 @@
 package use_case;
 
 public class LogoutOutputData {
+    private boolean fail;
+
+    //Constructor
+    public LogoutOutputData(boolean fail) {
+        this.fail = fail;
+    }
+
+    //Getters
+    public boolean isFail() {return this.fail;}
+
+    //Setter
+    public void setFail(boolean fail) {
+        this.fail = fail;
+    }
 }
+
