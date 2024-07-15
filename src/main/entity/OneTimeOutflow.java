aaa0e78fe9af36c2d1a310c737dd71e4f3462d07
chenj926 <shendicjl@gmail.com>
2024-07-15 04:31:30 -0400

    fix the ontime in out flow again to makr it up to date

--- src/main/entity/OneTimeOutflow.java
+++ src/main/entity/OneTimeOutflow.java
@@ -3,9 +3,8 @@ package entity;
 import java.time.LocalDate;
 
 public class OneTimeOutflow extends OneTimeTransaction {
-    public OneTimeOutflow(String identification, float amount, LocalDate date, String description) {
-        super(identification, amount, date, description);
-        if(this.isInflow()) System.out.println("Periodic outflow error, should be inflow!");
+    public OneTimeOutflow(String identification, float amount, LocalDate date, String description, String category) {
+        super(identification, amount, date, description, category);
     }
 }
 /*
