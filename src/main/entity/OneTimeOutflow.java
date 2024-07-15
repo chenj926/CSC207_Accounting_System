3fe6548d1f7219329522edf20522beb65f0b87aa
dana <danak.huang@mail.utoronto.ca>
2024-07-15 03:07:26 -0400

    fixed OneTimeInflow

--- src/main/entity/OneTimeOutflow.java
+++ src/main/entity/OneTimeOutflow.java
@@ -5,7 +5,6 @@ import java.time.LocalDate;
 public class OneTimeOutflow extends OneTimeTransaction {
     public OneTimeOutflow(String identification, float amount, LocalDate date, String description, String category) {
         super(identification, amount, date, description, category);
-        if(this.isInflow()) System.out.println("Periodic outflow error, should be inflow!");
     }
 }
 /*
