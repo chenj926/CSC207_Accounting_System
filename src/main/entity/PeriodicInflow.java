3fe873a2cadd82940b4dff89eb4fe1432bbb06e2
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-07-16 00:29:57 -0400

    deleted old versions left in Javadoc

--- src/main/entity/PeriodicInflow.java
+++ src/main/entity/PeriodicInflow.java
@@ -8,30 +8,3 @@ public class PeriodicInflow extends PeriodicTransaction {
         super(identification, amount, startDate, description, endDate, duration);
     }
 }
-
-/*
-public class PeriodicInflow extends PeriodicTransaction {
-    private String incomeSource;
-
-    public PeriodicInflow(float transactionAmount, String transactionDate, String transactionDescription, String recurrence, String incomeSource) {
-        super(transactionAmount, transactionDate, transactionDescription, recurrence);
-        this.incomeSource = incomeSource;
-    }
-
-    public String getIncomeSource() {
-        return incomeSource;
-    }
-
-    public void setIncomeSource(String incomeSource) {
-        this.incomeSource = incomeSource;
-    }
-
-    @Override
-    public void recordTransaction() {
-        System.out.println("Recording Income: ");
-        displayTransactionDetails();
-        System.out.println("Source: " + incomeSource);
-    }
-}
-
- */
--- src/main/entity/PeriodicOutflow.java
+++ src/main/entity/PeriodicOutflow.java
@@ -8,30 +8,3 @@ public class PeriodicOutflow extends PeriodicTransaction {
         super(identification, amount, startDate, description, endDate, duration);
     }
 }
-
-/*
-public class PeriodicOutflow extends PeriodicTransaction {
-    private String expenseCategory;
-
-    public PeriodicOutflow(float transactionAmount, String transactionDate, String transactionDescription, String recurrence, String expenseCategory) {
-        super();
-        this.expenseCategory = expenseCategory;
-    }
-
-    public String getExpenseCategory() {
-        return expenseCategory;
-    }
-
-    public void setExpenseCategory(String expenseCategory) {
-        this.expenseCategory = expenseCategory;
-    }
-
-    @Override
-    public void recordTransaction() {
-        System.out.println("Recording Expense: ");
-        displayTransactionDetails();
-        System.out.println("Category: " + expenseCategory);
-    }
-}
-
- */
