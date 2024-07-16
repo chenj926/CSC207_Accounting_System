4624202e568648097c36cab79e05d7f33fa20b58
chenj926 <shendicjl@gmail.com>
2024-07-16 19:05:08 -0400

    finished implementing PeriodicTransactionInteractor

--- src/main/use_case/PeriodicTransactionInteractor.java
+++ src/main/use_case/PeriodicTransactionInteractor.java
@@ -3,6 +3,7 @@ package use_case;
 import data_access.UserAccountDataAccessInterface;
 
 import entity.PeriodicInflow;
+import entity.PeriodicOutflow;
 import entity.UserAccount;
 
 import java.time.LocalDate;
@@ -25,6 +26,10 @@ public class PeriodicTransactionInteractor implements PeriodicTransactionInputBo
         this.userAccount = userAccount;
     }
 
+
+    public void executeHelper(PeriodicTransactionInputData periodicTransactionInputData, boolean isInflow) {
+
+    }
     @Override
     public void execute(PeriodicTransactionInputData periodicTransactionInputData) {
         String identification = periodicTransactionInputData.getIdentification();
@@ -48,6 +53,7 @@ public class PeriodicTransactionInteractor implements PeriodicTransactionInputBo
         int customPeriod = 0;
 
         // update the inflow and outflow
+        // for inflow
         if (isInflow) {
             float totalIncome = userAccount.getTotalIncome();
             income = totalIncome + amount;
@@ -124,6 +130,108 @@ public class PeriodicTransactionInteractor implements PeriodicTransactionInputBo
                     userAccount.setTotalBalance(totalBalance);
 
                     outputData = new PeriodicTransactionOutputData(periodicInflow,
+                            userAccount.getTotalBalance());
+                } else {
+                    // inflow transaction
+                    PeriodicInflow periodicInflow = new PeriodicInflow(identification, amount, localStartDate,
+                            description, localEndDate, customPeriod);
+                    // update the balance accordingly
+                    float balance = userAccount.getTotalBalance();
+                    float totalBalance = balance + income;
+                    userAccount.setTotalBalance(totalBalance);
+
+                    outputData = new PeriodicTransactionOutputData(periodicInflow,
+                            userAccount.getTotalBalance());
+                }
+
+                // Move to the next date based on the period
+                if (unit != ChronoUnit.DAYS) {
+                    currentDate = currentDate.plus(1, unit);  // increments currentDate by 1 unit
+                    outputData.setDate(currentDate);
+                } else {
+                    currentDate = currentDate.plusDays(customPeriod);  // plus the custom days
+                    outputData.setDate(currentDate);
+                }
+            }
+        }
+
+        // for outflow
+        else {
+            float totalIncome = userAccount.getTotalIncome();
+            income = totalIncome + amount;
+            userAccount.setTotalIncome(income);  // update the total income
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            LocalDate localStartDate = null;
+            LocalDate localEndDate = null;
+
+            try {
+                // now we have to convert both start and end date
+                localStartDate = LocalDate.parse(startDate, formatter);
+                localEndDate = LocalDate.parse(endDate, formatter);
+                if (localStartDate.isAfter(localEndDate)) {
+                    presenter.prepareFailView("Start date after end day, Plz enter again");
+                    return;
+                }
+            } catch (DateTimeParseException e) {  // if anyone of the start end has issue, catch!
+                presenter.prepareFailView("Invalid date format! Plz enter again");
+                return;
+            }
+
+            // if the user did not choose the pre-determined period
+            if (!periodTypes.contains(period)) {
+                try {
+                    // assume the custom period is in days
+                    customPeriod = Integer.parseInt(period);
+                } catch (NumberFormatException e) {
+                    presenter.prepareFailView("The custom period is not in correct format, please enter again!");
+                    return;  // fail than do not excute the rest of the code
+                }
+            }
+
+            ChronoUnit unit;
+            switch (period) {
+                case "day":
+                    unit = ChronoUnit.DAYS;
+                    break;
+                case "week":
+                    unit = ChronoUnit.WEEKS;
+                    break;
+                case "month":
+                    unit = ChronoUnit.MONTHS;
+                    break;
+                case "year":
+                    unit = ChronoUnit.YEARS;
+                    break;
+                default:
+                    unit = ChronoUnit.DAYS; // default to days if a custom period is provided
+                    break;
+            }
+
+            // Calculate the total number of days between start and end dates
+            long totalDaysBetween = ChronoUnit.DAYS.between(localStartDate, localEndDate);
+            // period is longer than the days between start and end
+            if ((unit != ChronoUnit.DAYS && totalDaysBetween < unit.getDuration().toDays()) ||
+                    (unit == ChronoUnit.DAYS && totalDaysBetween < customPeriod)) {
+                presenter.prepareFailView("Period is longer than the period between start and end date!");
+                return;
+            }
+
+            LocalDate currentDate = localStartDate;
+            while (!currentDate.isAfter(localEndDate)) {
+                // Add the transaction for the current date
+
+                PeriodicTransactionOutputData outputData;
+                if (periodTypes.contains(period)) {
+                    // inflow transaction
+                    PeriodicOutflow periodicOutflow = new PeriodicOutflow(identification, amount, localStartDate,
+                            description, localEndDate, (int) unit.getDuration().toDays());
+                    // update the balance accordingly
+                    float balance = userAccount.getTotalBalance();
+                    float totalBalance = balance + income;
+                    userAccount.setTotalBalance(totalBalance);
+
+                    outputData = new PeriodicTransactionOutputData(periodicOutflow,
                             userAccount.getTotalBalance());
                 } else {
                     // inflow transaction
