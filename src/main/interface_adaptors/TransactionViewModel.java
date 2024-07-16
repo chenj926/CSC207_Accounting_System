956734ab13facf7627b7713342d724fe9a6811df
xilechen <xilechen0111@gmail.com>
2024-07-16 16:23:00 -0400

    Added PropertyChangeSupport and button label constants to TransactionViewModel

--- src/main/interface_adaptors/TransactionViewModel.java
+++ src/main/interface_adaptors/TransactionViewModel.java
@@ -1,4 +1,4 @@
-package main.interface_adaptors;
+package interface_adaptors;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
@@ -43,6 +43,7 @@ public class TransactionViewModel {
         setCurrentViewModel(new PeriodicTransactionViewModel());
     }
 }
+
 
 
 
