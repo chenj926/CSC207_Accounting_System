7d18cf09af1303cb23cf5683a4c254602a46c96f
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-07-17 03:11:46 -0400

    added view labels and getters

--- src/main/interface_adaptors/TransactionViewModel.java
+++ src/main/interface_adaptors/TransactionViewModel.java
@@ -4,7 +4,51 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class TransactionViewModel extends ViewModel {
-    public static final String TRANSACTION_PROPERTY = "transaction";
+    // labels
+    private final String TRANSACTION_VIEW_LABEL = "Transaction";
+
+    private final String ONE_TIME_BUTTON_LABEL = "One Time Transaction";
+    private final String PERIODIC_BUTTON_LABEL = "Periodic Transaction";
+
+
+    private final String AMOUNT_LABEL = "Transaction Amount";
+    private final String DATE_LABEL = "Transaction Date";
+    private final String DESCRIPTION_LABEL = "Description";
+    private final String RECURRENCE_PERIOD_LABEL = "Recurrence Period";
+
+    private final String RECORD_BUTTON_LABEL = "Record Transaction";
+    private final String CANCEL_BUTTON_LABEL = "Cancel";
+
+    // label getters
+    public String getTransactionViewLabel() {
+        return TRANSACTION_VIEW_LABEL;
+    }
+    public String getOneTimeButtonLabel() {
+        return ONE_TIME_BUTTON_LABEL;
+    }
+    public String getPeriodicButtonLabel() {
+        return PERIODIC_BUTTON_LABEL;
+    }
+    public String getAmountLabel() {
+        return AMOUNT_LABEL;
+    }
+    public String getDateLabel() {
+        return DATE_LABEL;
+    }
+    public String getDescriptionLabel() {
+        return DESCRIPTION_LABEL;
+    }
+    public String getRecurrencePeriodLabel() {
+        return RECURRENCE_PERIOD_LABEL;
+    }
+    public String getRecordButtonLabel() {
+        return RECORD_BUTTON_LABEL;
+    }
+    public String getCancelButtonLabel() {
+        return CANCEL_BUTTON_LABEL;
+    }
+
+
     private final TransactionState transactionState = new TransactionState();
     protected TransactionViewModel currentViewModel;
 
