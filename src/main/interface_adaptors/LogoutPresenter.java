75a2c72b4e25a80f76ccb4df9e86b8c80779494d
dana <danak.huang@mail.utoronto.ca>
2024-07-17 04:32:38 -0400

    LogoutPresenter added

--- src/main/interface_adaptors/LogoutPresenter.java
+++ src/main/interface_adaptors/LogoutPresenter.java
@@ -1,4 +1,35 @@
 package interface_adaptors;
 
-public class LogoutPresenter {
+import use_case.LogoutOutputBoundary;
+import use_case.LogoutOutputData;
+import use_case.SignupOutputBoundary;
+import use_case.SignupOutputData;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LogoutPresenter implements LogoutOutputBoundary {
+    private final LogoutViewModel logoutViewModel;
+    private ViewManagerModel viewManagerModel;
+
+    public LogoutPresenter(LogoutViewModel logoutViewModel, ViewManagerModel viewManagerModel) {
+        this.logoutViewModel = logoutViewModel;
+        this.viewManagerModel = viewManagerModel;
+
+    }
+
+    @Override
+    public void prepareSuccessView(LogoutOutputData response) {
+        LogoutState logoutState = logoutViewModel.getState();
+        viewManagerModel.setActiveView(logoutViewModel.getViewName());
+        viewManagerModel.firePropertyChanged();
+
+    }
+
+    @Override public void prepareFailView(String message) {
+        LogoutState logoutState = logoutViewModel.getState();
+        logoutState.setLogoutMessage(message);
+        logoutViewModel.firePropertyChanged();
+    }
 }
+
