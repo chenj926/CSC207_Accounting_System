0da5c92f9630ee1ad786d83c000054de4f9ad500
xilechen <xilechen0111@gmail.com>
2024-07-16 20:19:45 -0400

    Refactor OneTimeTransactionViewModel

--- src/main/interface_adaptors/OneTimeTransactionViewModel.java
+++ src/main/interface_adaptors/OneTimeTransactionViewModel.java
@@ -1,83 +1,24 @@
 package interface_adaptors;
 
+import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class OneTimeTransactionViewModel extends TransactionViewModel {
-    public final String TITLE_LABEL = "One-Time Transaction";
-    public final String AMOUNT_LABEL = "Transaction Amount";
-    public final String DATE_LABEL = "Transaction Date";
-    public final String DESCRIPTION_LABEL = "Transaction Description";
-    public final String CATEGORY_LABEL = "Transaction Category";
-    public final String NOTES_LABEL = "Transaction Notes";
-    public final String RECORD_BUTTON_LABEL = "Record Transaction";
-    public final String CANCEL_BUTTON_LABEL = "Cancel";
+    private OneTimeTransactionState transactionState;
 
-    private float transactionAmount;
-    private String transactionDate;
-    private String transactionDescription;
-    private float newBalance;
-    private String transactionCategory;
-    private boolean useCaseFailed;
-    private String error;
+    public OneTimeTransactionViewModel() {
+        super("One-Time Transaction");
+        this.transactionState = new OneTimeTransactionState();
+    }
+
+    private OneTimeTransactionState state = new OneTimeTransactionState();
 
     public OneTimeTransactionViewModel() {
         super("One-Time Transaction");
     }
 
-    public float getTransactionAmount() {
-        return transactionAmount;
-    }
-
-    public void setTransactionAmount(float transactionAmount) {
-        this.transactionAmount = transactionAmount;
-    }
-
-    public String getTransactionDate() {
-        return transactionDate;
-    }
-
-    public void setTransactionDate(String transactionDate) {
-        this.transactionDate = transactionDate;
-    }
-
-    public String getTransactionDescription() {
-        return transactionDescription;
-    }
-
-    public void setTransactionDescription(String transactionDescription) {
-        this.transactionDescription = transactionDescription;
-    }
-
-    public float getNewBalance() {
-        return newBalance;
-    }
-
-    public void setNewBalance(float newBalance) {
-        this.newBalance = newBalance;
-    }
-
-    public String getTransactionCategory() {
-        return transactionCategory;
-    }
-
-    public void setTransactionCategory(String transactionCategory) {
-        this.transactionCategory = transactionCategory;
-    }
-
-    public boolean isUseCaseFailed() {
-        return useCaseFailed;
-    }
-
-    public void setUseCaseFailed(boolean useCaseFailed) {
-        this.useCaseFailed = useCaseFailed;
-    }
-
-    public String getError() {
-        return error;
-    }
-
-    public void setError(String error) {
-        this.error = error;
+    public void setState(OneTimeTransactionState state) {
+        this.state = state;
     }
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
@@ -85,15 +26,14 @@ public class OneTimeTransactionViewModel extends TransactionViewModel {
     // This is what the Signup Presenter will call to let the ViewModel know
     // to alert the View
     public void firePropertyChanged() {
-        support.firePropertyChange("transaction", null, this);
+        support.firePropertyChange("transaction", null, this.state);
     }
 
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
 
+    public OneTimeTransactionState getState() {
+        return state;
+    }
 }
-
-
-
-
-
-
-
