11f1aa18cf577952f2a8208880630ce87d0ca268
xilechen <xilechen0111@gmail.com>
2024-07-16 20:21:44 -0400

    Updated OneTimeTransactionPresenter

--- src/main/interface_adaptors/OneTimeTransactionPresenter.java
+++ src/main/interface_adaptors/OneTimeTransactionPresenter.java
@@ -5,7 +5,6 @@ import use_case.OneTimeTransactionOutputData;
 
 public class OneTimeTransactionPresenter implements OneTimeTransactionOutputBoundary {
     private final OneTimeTransactionViewModel viewModel;
-    private ViewManagerModel viewManagerModel;
 
     public OneTimeTransactionPresenter(OneTimeTransactionViewModel viewModel) {
         this.viewModel = viewModel;
@@ -18,15 +17,18 @@ public class OneTimeTransactionPresenter implements OneTimeTransactionOutputBoun
         viewModel.setTransactionDescription(data.getTransactionDescription());
         viewModel.setTransactionCategory(data.getTransactionCategory());
         viewModel.setUseCaseFailed(data.isUseCaseFailed());
-        viewModel.firePropertyChanged();
+        viewModel.setSuccessMessage("One-time transaction recorded successfully!");
+        viewModel.firePropertyChange();
     }
 
     @Override
-    public void prepareFailView(String message) {
-        viewModel.setError(message);
-        viewModel.firePropertyChanged();
+    public void prepareFailView(String error) {
+        viewModel.setError(error);
+        viewModel.setSuccessMessage(null); // Clear success message on failure
+        viewModel.firePropertyChange();
     }
 }
+
 
 
 
