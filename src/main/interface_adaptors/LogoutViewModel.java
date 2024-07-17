271d849f8018d30d460b8ac67dc1fcbc4a109ca8
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-07-17 02:39:47 -0400

    encapusalated labels

--- src/main/interface_adaptors/LogoutViewModel.java
+++ src/main/interface_adaptors/LogoutViewModel.java
@@ -4,12 +4,23 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class LogoutViewModel extends ViewModel {
-    public final String titleLabel = "LogoutView";
-    public final String logoutButtonText = "Logout";
-    public final String cancelButtonText = "Cancel";
+    private final String titleLabel = "LogoutView";
+    private final String logoutButtonText = "Logout";
+    private final String cancelButtonText = "Cancel";
     private LogoutState state = new LogoutState();
 
     private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    // label getters
+    public String getTitleLabel() {
+        return titleLabel;
+    }
+    public String getLogoutButtonText() {
+        return logoutButtonText;
+    }
+    public String getCancelButtonText() {
+        return cancelButtonText;
+    }
 
     public LogoutViewModel() {
         super("logout");
@@ -24,4 +35,5 @@ public class LogoutViewModel extends ViewModel {
     public void addPropertyChangeListener(PropertyChangeListener listener){
         support.addPropertyChangeListener(listener);
     }
+
 }
