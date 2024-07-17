9f2a7bdff5177f08f093bad724444ca75ddafae7
xilechen <xilechen0111@gmail.com>
2024-07-17 17:37:56 -0400

    Updated TransactionViewModel for input form restrictions

--- src/main/interface_adaptors/TransactionViewModel.java
+++ src/main/interface_adaptors/TransactionViewModel.java
@@ -85,6 +85,12 @@ public class TransactionViewModel extends ViewModel {
         firePropertyChanged("currentViewModel", oldViewModel, newViewModel);
     }
 
+    public void resetTransactionView() {
+        transactionState.setOneTimeSelected(false);
+        transactionState.setPeriodicSelected(false);
+        setCurrentViewModel(null);
+    }
+
     public void cancelTransaction() {
         setCurrentViewModel(null); // This can be adjusted based on how you want to clear the view
         firePropertyChanged("currentViewModel", null, this);
