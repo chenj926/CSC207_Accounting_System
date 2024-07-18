8de42d6b18c8141414a4a83a6947f4a2e4224a74
Rita Wang <ritaaawzy@gmail.com>
2024-07-18 18:11:53 -0400

    Modified the test for LogoutState

--- src/test/interface_adaptors/LogoutStateTest.java
+++ src/test/interface_adaptors/LogoutStateTest.java
@@ -1,2 +1,16 @@
 package interface_adaptors;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class LogoutStateTest {
+
+    @Test
+    void testDefaultConstructor() {
+        LogoutState logoutState = new LogoutState();
+        Assertions.assertEquals("", logoutState.getUserId());
+        Assertions.assertEquals("confirm to logout", logoutState.getLogoutMessage());
+        Assertions.assertFalse(logoutState.isLoggedOut());
+    }
+
+}
