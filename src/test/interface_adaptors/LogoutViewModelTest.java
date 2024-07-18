293cf75c2f07941ed13b10a7827abd71a43abefc
Rita Wang <ritaaawzy@gmail.com>
2024-07-18 18:23:39 -0400

    Modified the test for LogoutViewModel

--- src/test/interface_adaptors/LogoutViewModelTest.java
+++ src/test/interface_adaptors/LogoutViewModelTest.java
@@ -1,1 +1,24 @@
 package interface_adaptors;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class LogoutViewModelTest {
+
+    private LogoutViewModel logoutViewModel;
+
+    @BeforeEach
+    void setUp() {
+        logoutViewModel = new LogoutViewModel();
+    }
+
+    @Test
+    void testConstructor() {
+        Assertions.assertEquals("LogoutView", logoutViewModel.getTitleLabel());
+        Assertions.assertEquals("Logout", logoutViewModel.getLogoutButtonText());
+        Assertions.assertEquals("Cancel", logoutViewModel.getCancelButtonText());
+        Assertions.assertNotNull(logoutViewModel.getState());
+    }
+}
