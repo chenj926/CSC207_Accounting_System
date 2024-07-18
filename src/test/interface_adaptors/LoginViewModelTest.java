918ac5b9baa2d3c2ea588e915422b3045a2d68a3
Rita Wang <ritaaawzy@gmail.com>
2024-07-18 17:37:08 -0400

    Modified the test for LoginViewModel

--- src/test/interface_adaptors/LoginViewModelTest.java
+++ src/test/interface_adaptors/LoginViewModelTest.java
@@ -1,1 +1,29 @@
 package interface_adaptors;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+class LoginViewModelTest {
+
+    private LoginViewModel loginViewModel;
+
+    @BeforeEach
+    void setUp() {
+        loginViewModel = new LoginViewModel();
+    }
+
+    @Test
+    void testGetters() {
+        assertEquals("Log In View", loginViewModel.getTitleLabel());
+        assertEquals("Enter username", loginViewModel.getUsernameLabel());
+        assertEquals("Enter password", loginViewModel.getPasswordLabel());
+        assertEquals("Log in", loginViewModel.getLoginButtonLabel());
+        assertEquals("Cancel", loginViewModel.getCancelButtonLabel());
+    }
+}
