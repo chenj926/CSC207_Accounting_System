be0bb9124f7d42d56328c0d08424987ed9f1c918
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-07-18 18:51:29 -0400

    added Login use case factory

---
+++ src/main/app/LoginUseCaseFactory.java
@@ -0,0 +1,35 @@
+package app;
+
+import data_access.*;
+import entity.*;
+import interface_adaptors.*;
+import use_case.*;
+import view.*;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class LoginUseCaseFactory {
+
+    private LoginUseCaseFactory() {}
+
+    public static LoginView create(ViewManagerModel viewManagerModel, LoginViewModel loginViewModel) {
+        try {
+            LoginController loginController = createUserLoginUseCase(viewManagerModel, loginViewModel);
+            return new LoginView(loginViewModel, loginController);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Could not open user data file.");
+        }
+        return null;
+    }
+
+    private static LoginController createUserLoginUseCase(ViewManagerModel viewManagerModel, LoginViewModel loginViewModel) throws IOException {
+        LoginDataAccessInterface dataAccessObject = (LoginDataAccessInterface) DAOFactory.getUserSignupDataAccessObject();
+
+        LoginOutputBoundary presenter = new LoginPresenter(viewManagerModel, loginViewModel);
+
+        LoginInteractor interactor = new LoginInteractor(dataAccessObject, presenter);
+
+        return new LoginController(interactor);
+    }
+}
