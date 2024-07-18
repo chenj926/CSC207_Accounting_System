30b12848ef15f32aa1ff85be6e696e4a6750794e
Rita Wang <ritaaawzy@gmail.com>
2024-07-18 18:57:20 -0400

    Modified the test for SignupViewModel

--- src/test/interface_adaptors/SignupViewModelTest.java
+++ src/test/interface_adaptors/SignupViewModelTest.java
@@ -1,1 +1,27 @@
 package interface_adaptors;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+
+class SignupViewModelTest {
+
+    private SignupViewModel signupViewModel;
+
+    @BeforeEach
+    void setUp() {
+        signupViewModel = new SignupViewModel();
+    }
+
+    @Test
+    void testGetters() {
+        Assertions.assertEquals("Sign Up View", signupViewModel.getTitleLabel());
+        Assertions.assertEquals("Set username", signupViewModel.getUsernameLabel());
+        Assertions.assertEquals("Set password", signupViewModel.getPasswordLabel());
+        Assertions.assertEquals("Set identification", signupViewModel.getID_LABEL());
+        Assertions.assertEquals("Sign up", signupViewModel.getSignupButtonLabel());
+        Assertions.assertEquals("Cancel", signupViewModel.getCancelButtonLabel());
+    }
+
+}
