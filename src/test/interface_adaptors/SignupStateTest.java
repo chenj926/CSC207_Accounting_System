357600325905a0e2e42c0ca073a5992f24f12c54
Rita Wang <ritaaawzy@gmail.com>
2024-07-18 18:35:49 -0400

    Modified the test for SignupState

--- src/test/interface_adaptors/SignupStateTest.java
+++ src/test/interface_adaptors/SignupStateTest.java
@@ -1,1 +1,26 @@
 package interface_adaptors;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SignupStateTest {
+
+    private SignupState signupState;
+
+    @BeforeEach
+    void setUp() {
+        signupState = new SignupState();
+    }
+
+    @Test
+    void testConstructor() {
+        Assertions.assertEquals("", signupState.getIdentification());
+        Assertions.assertNull(signupState.getIdentificationError());
+        Assertions.assertEquals("", signupState.getUsername());
+        Assertions.assertNull(signupState.getUsernameError());
+        Assertions.assertEquals("", signupState.getPassword());
+        Assertions.assertNull(signupState.getPasswordError());
+    }
+
+}
