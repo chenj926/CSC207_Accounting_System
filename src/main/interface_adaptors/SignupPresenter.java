4614a4e4b8450e8d292f9e77d8999207139ccc9e
chenj926 <shendicjl@gmail.com>
2024-07-18 03:48:44 -0400

    seperate signup with login

--- src/main/interface_adaptors/SignupPresenter.java
+++ src/main/interface_adaptors/SignupPresenter.java
@@ -9,31 +9,30 @@ import java.time.format.DateTimeFormatter;
 public class SignupPresenter implements SignupOutputBoundary {
 
     private final SignupViewModel signupViewModel;
-    private final LoginViewModel loginViewModel;
     private ViewManagerModel viewManagerModel;
 
     public SignupPresenter(ViewManagerModel viewManagerModel,
-                           SignupViewModel signupViewModel,
-                           LoginViewModel loginViewModel) {
+                           SignupViewModel signupViewModel) {
         this.viewManagerModel = viewManagerModel;
         this.signupViewModel = signupViewModel;
-        this.loginViewModel = loginViewModel;
     }
 
     @Override
     public void prepareSuccessView(SignupOutputData response) {
         // On success, switch to the login view.
+        System.out.println("prepareSuccessView called with response: " + response.getUsername());
 
         SignupState signupState = signupViewModel.getState();
-        LoginState loginState = loginViewModel.getState();
-        loginState.setUsername(response.getUsername());
-        this.loginViewModel.setState(loginState);
-        loginViewModel.firePropertyChanged();
-        viewManagerModel.setActiveViewName(loginViewModel.getViewName());
+//        LoginState loginState = loginViewModel.getState();
+        signupState.setUsername(response.getUsername());
+        this.signupViewModel.setState(signupState);
+        signupViewModel.firePropertyChanged();
+        viewManagerModel.setActiveViewName(signupViewModel.getViewName());
     }
 
     @Override
     public void prepareFailView(String error) {
+        System.out.println("prepareFailView called with error: " + error);
         SignupState signupState = signupViewModel.getState();
         signupState.setUsernameError(error);
         signupViewModel.firePropertyChanged();
