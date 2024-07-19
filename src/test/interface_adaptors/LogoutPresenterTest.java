21d7e4f86c93bebf5cc05e6dea5fd6bb042d1def
Rita Wang <ritaaawzy@gmail.com>
2024-07-18 21:58:05 -0400

    Created test for LogoutPresenter

--- src/test/interface_adaptors/LogoutPresenterTest.java
+++ src/test/interface_adaptors/LogoutPresenterTest.java
@@ -1,1 +1,14 @@
 package interface_adaptors;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import use_case.LogoutOutputData;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LogoutPresenterTest {
+    private LogoutViewModel logoutViewModel;
+    private ViewManagerModel viewManagerModel;
+    private LogoutPresenter logoutPresenter;
+
+}
