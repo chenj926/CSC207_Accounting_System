9b48305899029167feda56f90c67be17c164201c
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-07-19 00:04:52 -0400

    added in mem loginout object

---
+++ src/main/data_access/InMemoryLoginoutDataAccessObject.java
@@ -0,0 +1,32 @@
+package data_access;
+
+import entity.*;
+import java.util.*;
+
+public class InMemoryLoginoutDataAccessObject extends InMemoryUserAccountDataAccessObject implements LogoutDataAccessInterface, LoginDataAccessInterface {
+    private static Map<String, Boolean> userLogin;
+
+    public InMemoryLoginoutDataAccessObject() {
+        userLogin = new HashMap<>();
+    }
+
+    @Override
+    public boolean existById(String identification) {
+        return userLogin.containsKey(identification);
+    }
+
+    @Override
+    public void login(UserAccount userAccount) {
+        userLogin.put(userAccount.getIdentification(), true);
+    }
+
+    @Override
+    public void logout(UserAccount user) {
+        userLogin.put(user.getIdentification(), false);
+    }
+
+    @Override
+    public UserAccount getById(String identification) {
+        return super.getById(identification);
+    }
+}
