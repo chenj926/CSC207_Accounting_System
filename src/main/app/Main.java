0b8a9f5de5382111408ab3cdd85907e8b028cdf9
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-07-18 23:19:09 -0400

    added main

---
+++ src/main/app/Main.java
@@ -0,0 +1,24 @@
+package app;
+
+import interface_adaptors.*;
+        import view.*;
+
+        import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+
+            ViewManagerModel viewManagerModel = new ViewManagerModel();
+            viewManagerModel.changeView("sign up");
+
+
+            viewManagerModel.addPropertyChangeListener(evt -> {
+                String newViewName = (String) evt.getNewValue();
+                viewManagerModel.changeView(newViewName);
+            });
+
+
+        });
+    }
+}
