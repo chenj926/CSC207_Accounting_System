38513222eb676bd661d6d8ecaf9b43ddbfd7abbe
dana <danak.huang@mail.utoronto.ca>
2024-07-20 07:01:49 -0400

    homePageViewModel added

--- src/main/interface_adaptors/HomePageViewModel.java
+++ src/main/interface_adaptors/HomePageViewModel.java
@@ -1,2 +1,48 @@
-package interface_adaptors;public class HomePageViewModel {
-}
+package interface_adaptors;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class HomePageViewModel extends ViewModel{
+
+    private final String titleLabel = "Home Page";
+    private final String signupButtonLabel = "Sign up";
+    private final String loginButtonLabel = "Login";
+    private final String exitButtonLabel = "Exit";
+
+    // private HomePageState state = new HomePageState();
+
+    public HomePageViewModel() {
+        super("Home Page");
+    }
+
+    // getters
+    public String getTitleLabel() {
+        return this.titleLabel;
+    }
+    public String getSignupButtonLabel() {
+        return this.signupButtonLabel;
+    }
+    public String getLoginButtonLabel() {
+        return this.loginButtonLabel;
+    }
+    public String getExitButtonLabel() {
+        return this.exitButtonLabel;
+    }
+
+    // public HomePageState getState() {
+      //   return this.state;}
+
+    // setter
+    // public void setState(HomePageState state) { this.state = state; }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+
+    //
+    // public void firePropertyChanged() {
+    //    support.firePropertyChange("state", null);}
+
+    // public void addPropertyChangeListener(PropertyChangeListener listener) {
+    //    support.addPropertyChangeListener(listener);
+    }
