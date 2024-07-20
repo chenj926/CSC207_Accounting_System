2d74c9c6a34609028cc24674f50d262f2d8faf97
dana <danak.huang@mail.utoronto.ca>
2024-07-20 07:02:41 -0400

    homePageUseCaseFactory added

--- src/main/app/HomePageUseCaseFactory.java
+++ src/main/app/HomePageUseCaseFactory.java
@@ -1,2 +1,21 @@
-package app;public class HomePageUseCaseFactory {
+package app;
+
+import data_access.*;
+import entity.*;
+import interface_adaptors.*;
+import use_case.*;
+import view.*;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class HomePageUseCaseFactory {
+
+    private HomePageUseCaseFactory() {}
+
+    public static HomePageView create(ViewManagerModel viewManagerModel, HomePageViewModel homePageViewModel){
+        try {
+
+        }catch ()
+    }
 }
