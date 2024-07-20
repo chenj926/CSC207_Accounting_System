60f98497fea98e51c36d0b88636fa1e920d8c693
dana <danak.huang@mail.utoronto.ca>
2024-07-20 01:43:25 -0400

    fix logoutState

--- src/main/interface_adaptors/LogoutState.java
+++ src/main/interface_adaptors/LogoutState.java
@@ -1,22 +1,16 @@
 package interface_adaptors;
 
 public class LogoutState {
-    private String userId = "";
-    private String logoutMessage = "confirm to logout";
+    private String logoutMessage;
     private boolean isLoggedOut = false;
 
-    public LogoutState(LogoutState copy) {
-        userId = copy.userId;
-        logoutMessage = copy.logoutMessage;
-        isLoggedOut = copy.isLoggedOut;
+    public LogoutState() {
+        this.logoutMessage = null;
+        this.isLoggedOut =  false;
     }
 
     //the default constructor
-    public LogoutState() {}
 
-    public String getUserId() {
-        return userId;
-    }
 
     public String getLogoutMessage() {
         return logoutMessage;
@@ -26,9 +20,6 @@ public class LogoutState {
         return isLoggedOut;
     }
 
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
 
     public void setLogoutMessage(String logoutMessage) {
         this.logoutMessage = logoutMessage;
