9e47d22ace3514e9b44902413edf2ca25044855e
chifonghuang <chifong.huang@mail.utoronto.ca>
2024-07-29 17:20:06 -0400

    added javadoc to generate the FinancialReportInputBoundary

--- src/main/use_case/FinancialReport/FinancialReportInputBoundary.java
+++ src/main/use_case/FinancialReport/FinancialReportInputBoundary.java
@@ -1,5 +1,11 @@
 package use_case.FinancialReport;
 
+/**
+ * Input boundary interface for generating financial reports.
+ *
+ * Author: Chi Fong Huang
+ */
 public interface FinancialReportInputBoundary {
     void generateReport(FinancialReportInputData inputData);
 }
+
