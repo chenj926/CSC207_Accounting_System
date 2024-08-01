cfa6f12c6451e0815b8ba8563e5c2ecdbe6de616
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-08-01 19:10:38 -0400

    updated interactors to match new parameters

--- src/main/use_case/transaction/periodic/PeriodicTransactionInteractor.java
+++ src/main/use_case/transaction/periodic/PeriodicTransactionInteractor.java
@@ -346,7 +346,7 @@ public class PeriodicTransactionInteractor implements PeriodicTransactionInputBo
         userAccount.setTotalBalance(totalBalance);
 
         // Prepare the output data
-        PeriodicTransactionOutputData outputData = new PeriodicTransactionOutputData(periodicOutflow, totalBalance);
+        PeriodicTransactionOutputData outputData = new PeriodicTransactionOutputData(periodicOutflow);
         userDataAccessObject.saveTransaction(null, outputData, true);
         userDataAccessObject.update(userAccount);
 
