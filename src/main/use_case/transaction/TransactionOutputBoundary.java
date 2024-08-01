31b7e03b8398eb3f3948b845ed4b1c78bce4ae7d
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-08-01 19:04:54 -0400

    extended periodic and onetime transaction output boundary from TransactionOutputBoundary

--- src/main/use_case/transaction/TransactionOutputBoundary.java
+++ src/main/use_case/transaction/TransactionOutputBoundary.java
@@ -1,5 +1,11 @@
 package use_case.transaction;
 
+/**
+ * The OTransactionOutputBoundary interface provides methods for preparing the view based on the outcome of a transaction operation.
+ * Implementations of this interface will handle the presentation logic for successful and failed transaction attempts.
+ *
+ * @author Jessica
+ */
 public interface TransactionOutputBoundary<TransactionOutputData>{
     /**
      * Prepares the success view with the given transaction output data.
--- src/main/use_case/transaction/one_time/OneTimeTransactionOutputBoundary.java
+++ src/main/use_case/transaction/one_time/OneTimeTransactionOutputBoundary.java
@@ -1,23 +1,14 @@
 package use_case.transaction.one_time;
 
+import use_case.transaction.TransactionOutputBoundary;
+
 /**
  * The OneTimeTransactionOutputBoundary interface provides methods for preparing the view based on the outcome of a one-time transaction operation.
  * Implementations of this interface will handle the presentation logic for successful and failed one-time transaction attempts.
  *
  * @author Dana
+ * @author Jessica
  */
-public interface OneTimeTransactionOutputBoundary{
-    /**
-     * Prepares the success view with the given one-time transaction output data.
-     *
-     * @param transactions the one-time transaction output data containing transaction information and new balance
-     */
-    void prepareSuccessView(OneTimeTransactionOutputData transactions);
-
-    /**
-     * Prepares the fail view with the given error message.
-     *
-     * @param error the error message to be presented in case of a failed one-time transaction attempt
-     */
-    void prepareFailView(String error);
+public interface OneTimeTransactionOutputBoundary extends TransactionOutputBoundary<OneTimeTransactionOutputData>{
+    // methods defined in TransactionOutputBoundary interface
 }
