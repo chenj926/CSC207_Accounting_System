8b61d7f4d748e6300939513c237c88a1fca86a74
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-08-01 15:32:17 -0400

    added transaction input data

---
+++ src/main/use_case/transaction/TransactionInputData.java
@@ -0,0 +1,52 @@
+package use_case.transaction;
+
+import java.time.LocalDate;
+
+public abstract class TransactionInputData {
+    private String transactionAmount;
+    private String transactionDescription;
+    private String transactionCategory;
+
+    /**
+     * Co   nstructs a TransactionInputData object with the specified details.
+     *
+     * @param transactionAmount      the amount of the transaction
+     * @param transactionDescription the description of the transaction
+     * @param transactionCategory    the category of the transaction
+     */
+    public TransactionInputData(String transactionAmount,
+                                String transactionDescription,
+                                String transactionCategory) {
+        this.transactionAmount = transactionAmount;
+        this.transactionDescription = transactionDescription;
+        this.transactionCategory = transactionCategory;
+
+    }
+
+    /**
+     * Gets the transaction amount.
+     *
+     * @return the transaction amount
+     */
+    public String getTransactionAmount() {
+        return this.transactionAmount;
+    }
+
+    /**
+     * Gets the description of the transaction.
+     *
+     * @return the description of the transaction
+     */
+    public String getTransactionDescription() {
+        return transactionDescription;
+    }
+
+    /**
+     * Gets the transaction category.
+     *
+     * @return the transaction category
+     */
+    public String getTransactionCategory() {
+        return this.transactionCategory;
+    }
+}
