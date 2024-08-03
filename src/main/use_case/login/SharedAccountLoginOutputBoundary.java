5ea1f9586e13bbcfe2f9cf023d12161829e17555
dana <danak.huang@mail.utoronto.ca>
2024-08-03 04:22:23 -0400

    SharedAccount input output boundary added

---
+++ src/main/use_case/login/SharedAccountLoginOutputBoundary.java
@@ -0,0 +1,25 @@
+package use_case.login;
+
+/**
+ * The LoginOutputBoundary interface provides methods for preparing the view based on the outcome of a login operation.
+ * Implementations of this interface will handle the presentation logic for successful and failed login attempts.
+ *
+ * @author Dana
+ */
+public interface SharedAccountLoginOutputBoundary {
+
+    /**
+     * Prepares the success view with the given sharedAccount login output data.
+     *
+     * @param user the login output data containing user information and success status
+     */
+    void prepareSuccessView(SharedAccountLoginInputData user);
+
+    /**
+     * Prepares the fail view with the given error message.
+     *
+     * @param error the error message to be presented in case of a failed login attempt
+     */
+    void prepareFailView(String error);
+
+}
