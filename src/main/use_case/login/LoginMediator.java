27f3b385d23cade3c126718104839d89ef8e35b0
chenz623 <jchenwz.chen@mail.utoronto.ca>
2024-08-04 06:52:15 -0400

    changed mediator to check login success before calling periodic interactor

--- src/main/use_case/login/LoginMediator.java
+++ src/main/use_case/login/LoginMediator.java
@@ -2,18 +2,16 @@ package use_case.login;
 
 import use_case.transaction.periodic.PeriodicTransactionInputBoundary;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.ResolverStyle;
 
 /**
  * The LoginMediator class mediates the interaction between the login process and periodic transaction updates.
+ * It orchestrates actions post-login, like updating periodic transactions.
  *
  * @author Jessica
  */
 public class LoginMediator {
     private final LoginInputBoundary loginInteractor;
     private final PeriodicTransactionInputBoundary periodicTransactionInteractor;
-    private final DateTimeFormatter formatter;
 
     /**
      * Constructs a LoginMediator object with the specified interactors.
@@ -21,19 +19,20 @@ public class LoginMediator {
      * @param loginInteractor the login interactor
      * @param periodicTransactionInteractor the periodic transaction interactor
      */
-    public LoginMediator(LoginInputBoundary loginInteractor, PeriodicTransactionInputBoundary periodicTransactionInteractor) {
+    public LoginMediator(LoginInputBoundary loginInteractor, PeriodicTransactionInputBoundary periodicTransactionInteractor, LoginOutputBoundary loginOutput) {
         this.loginInteractor = loginInteractor;
         this.periodicTransactionInteractor = periodicTransactionInteractor;
-        this.formatter = DateTimeFormatter.ofPattern("dd-MM-uuuu").withResolverStyle(ResolverStyle.STRICT);
     }
 
     public void execute(LoginInputData loginInputData) {
         loginInteractor.execute(loginInputData);
+    }
 
-        LocalDate currentDate = LocalDate.now();
-        String currentDateStr = currentDate.format(formatter);
-
-        // periodicTransactionInteractor.updateOnLogin(currentDateStr);
+    public void notifyLoginResult(boolean success, String userId) {
+        if (success) {
+            LocalDate currentDate = LocalDate.now();
+            // periodicTransactionInteractor.updateTransactionsBasedOnDate(userId, currentDate);
+        }
     }
 }
 
