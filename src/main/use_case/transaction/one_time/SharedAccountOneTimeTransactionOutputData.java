d166b2127cd41e02b95b2901175664d109547edb
Rita Wang <ritaaawzy@gmail.com>
2024-08-05 16:18:43 -0400

    Modified the shared account onetime transaction output data use case.

--- src/main/use_case/transaction/one_time/SharedAccountOneTimeTransactionOutputData.java
+++ src/main/use_case/transaction/one_time/SharedAccountOneTimeTransactionOutputData.java
@@ -4,3 +4,32 @@ import entity.account.SharedAccount;
 import entity.transaction.one_time.OneTimeTransaction;
 import use_case.transaction.TransactionOutputData;
 
+/**
+ * The SharedAccountOneTimeTransactionOutputData class represents the output data of a one-time transaction operation
+ * that involves a shared account.
+ * It includes details such as the new balance of the shared account, transaction amount, date, description, category, and status of the use case.
+ *
+ */
+public class SharedAccountOneTimeTransactionOutputData extends OneTimeTransactionOutputData {
+    private float newSharedAccountBalance;
+
+    /**
+     * Constructs a SharedAccountOneTimeTransactionOutputData object with the specified details.
+     *
+     * @param oneTimeTransaction the one-time transaction entity
+     * @param newSharedAccountBalance the new balance of the shared account
+     */
+    public SharedAccountOneTimeTransactionOutputData(OneTimeTransaction oneTimeTransaction, float newSharedAccountBalance) {
+        super(oneTimeTransaction);
+        this.newSharedAccountBalance = newSharedAccountBalance;
+    }
+
+    /**
+     * Gets the new balance of the shared account.
+     *
+     * @return the new balance of the shared account
+     */
+    public float getNewSharedAccountBalance() {
+        return newSharedAccountBalance;
+    }
+}
