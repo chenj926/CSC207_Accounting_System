664d03a365e1f5499b2afc53756a80b898967363
Rita Wang <ritaaawzy@gmail.com>
2024-08-05 16:18:15 -0400

    Modified the shared account onetime transaction output data use case.

---
+++ src/main/use_case/transaction/one_time/SharedAccountOneTimeTransactionOutputData.java
@@ -0,0 +1,6 @@
+package use_case.transaction.one_time;
+
+import entity.account.SharedAccount;
+import entity.transaction.one_time.OneTimeTransaction;
+import use_case.transaction.TransactionOutputData;
+
