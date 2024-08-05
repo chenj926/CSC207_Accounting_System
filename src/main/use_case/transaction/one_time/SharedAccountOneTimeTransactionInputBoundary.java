bdc1a3b8173762c3e6f74529b1922aa99368e27c
Rita Wang <ritaaawzy@gmail.com>
2024-08-05 15:59:03 -0400

    Modified the shared account onetime transaction input boundary use case.

---
+++ src/main/use_case/transaction/one_time/SharedAccountOneTimeTransactionInputBoundary.java
@@ -0,0 +1,20 @@
+package use_case.transaction.one_time;
+
+import use_case.transaction.TransactionInputBoundary;
+
+/**
+ * The SharedAccountOneTimeTransactionInputBoundary interface provides a method for executing one-time transaction operations
+ * that involve a shared account.
+ * Implementations of this interface will handle the process of creating a one-time transaction using the provided input data,
+ * with additional logic for handling shared accounts.
+ *
+ */
+public interface SharedAccountOneTimeTransactionInputBoundary extends TransactionInputBoundary<SharedAccountOneTimeTransactionInputData> {
+    /**
+     * Executes the one-time transaction operation for a shared account.
+     *
+     * @param inputData the input data for the one-time transaction operation
+     * @return the result of the one-time transaction operation
+     */
+    Object execute(SharedAccountOneTimeTransactionInputData inputData);
+}
