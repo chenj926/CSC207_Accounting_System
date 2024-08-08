ddd9915d117deaa2a582cf5a2769f4adc0f2262a
dana <danak.huang@mail.utoronto.ca>
2024-08-08 05:30:58 -0400

    financial report abstract class added and shared acc financial report added for input data

--- src/main/use_case/financial_report/FinancialReportInputData.java
+++ src/main/use_case/financial_report/FinancialReportInputData.java
@@ -1,4 +1,31 @@
 package use_case.financial_report;
 
-public class FinancialReportInputData {
+import java.util.Date;
+
+/**
+ * Input data for generating financial reports.
+ *
+ * @author :Dana
+ */
+public abstract class FinancialReportInputData {
+    private final String identification;
+
+
+    /**
+     * Constructs a FinancialReportInputData object.
+     *
+     * @param identification the account ID
+     */
+    public FinancialReportInputData(String identification) {
+        this.identification = identification;
+    }
+
+    /**
+     * Returns the account ID.
+     *
+     * @return the account ID
+     */
+    public String getIdentification() {
+        return this.identification;
+    }
 }
--- src/main/use_case/financial_report/SharedAccountFinancialReportInputData.java
+++ src/main/use_case/financial_report/SharedAccountFinancialReportInputData.java
@@ -1,4 +1,21 @@
 package use_case.financial_report;
 
-public class SharedAccountFinancialReportInputData {
+import use_case.login.SharedAccountLoginInputData;
+
+/**
+ * Input data for generating financial reports.
+ *
+ * @author :Dana
+ */
+public class SharedAccountFinancialReportInputData extends FinancialReportInputData {
+
+    /**
+     * Constructs a FinancialReportInputData object.
+     *
+     * @param identification the account ID
+     */
+
+    public SharedAccountFinancialReportInputData(String identification) {
+        super(identification);
+    }
 }
