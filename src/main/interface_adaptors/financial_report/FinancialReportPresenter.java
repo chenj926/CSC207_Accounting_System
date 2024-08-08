cd91a003b447d33e31fba98d6d1343645246818a
dana <danak.huang@mail.utoronto.ca>
2024-08-08 12:27:17 -0400

    financial report presenter abstract class added

--- src/main/interface_adaptors/financial_report/FinancialReportPresenter.java
+++ src/main/interface_adaptors/financial_report/FinancialReportPresenter.java
@@ -3,19 +3,22 @@ package interface_adaptors.financial_report;
 import interface_adaptors.ViewManagerModel;
 import use_case.financial_report.UserAccountFinancialReportOutputData;
 
+
 /**
  * Implements the output boundary to present the financial report.
  *
  * @author Dana
  */
 
-public abstract class FinancialReportPresenter {
-    private String reportContent;
-    private final ViewManagerModel viewManager;
+public abstract class FinancialReportPresenter<
+        V extends FinancialReportViewModel,
+        S extends FinancialReportState> {
+    protected final V viewModel;
 
+    protected String reportContent;
 
-    public FinancialReportPresenter(ViewManagerModel viewManager) {
-        this.viewManager = viewManager;
+    public FinancialReportPresenter(V viewModel, ViewManagerModel viewManager) {
+        this.viewModel = viewModel;
     }
 
 
@@ -24,6 +27,9 @@ public abstract class FinancialReportPresenter {
      *
      * @param outputData the output data containing the report content
      */
-    public void prepareSuccessView(UserAccountFinancialReportOutputData outputData) {
+    public void prepareSuccessView(FinancialReportOutputData outputData) {
         this.reportContent = outputData.getReportContent();
+        V state = (V) viewModel.getState();
+
+
 }
