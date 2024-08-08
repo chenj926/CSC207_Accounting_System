e86883606b085b51ebcd496318515ed475a8f4c0
dana <danak.huang@mail.utoronto.ca>
2024-08-08 05:44:31 -0400

    shared account financial report ouput addded and abstract class added

--- src/main/use_case/financial_report/SharedAccountFinancialReportOutputData.java
+++ src/main/use_case/financial_report/SharedAccountFinancialReportOutputData.java
@@ -1,4 +1,19 @@
 package use_case.financial_report;
 
-public class SharedAccountFinancialReportOutputData {
+/**
+ * Output data for presenting financial reports.
+ *
+ * @author :Dana
+ */
+public class SharedAccountFinancialReportOutputData extends FinancialReportOutputData {
+
+    /**
+     * Constructs a FinancialReportOutputData object.
+     *
+     * @param reportContent the content of the financial report
+     */
+    public SharedAccountFinancialReportOutputData(String reportContent) {
+
+        super(reportContent);
+    }
 }
