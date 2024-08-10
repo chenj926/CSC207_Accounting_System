ded175fcfd37ca77ea799b7f5f9ee9e64f2f0f33
chifonghuang <chifong.huang@mail.utoronto.ca>
2024-08-10 13:10:24 -0400

    fixed test bug

--- src/test/use_case/LoginInteractorTest.java
+++ src/test/use_case/LoginInteractorTest.java
@@ -22,19 +22,40 @@ public class LoginInteractorTest {
     private LoginDataAccessInterface userDataAccessObject;
     private SimplePresenter presenter;
     private LoginInteractor loginInteractor;
+    private class ConcreteloginInteractor extends LoginInteractor{
+
+        public ConcreteloginInteractor(Object userDataAccessObject, LoginOutputBoundary presenter) {
+            super(userDataAccessObject, presenter);
+        }
+
+        @Override
+        public void execute(LoginInputData inputData) {
+
+        }
+    }
+    private class ConcreteloginLoginInputData extends LoginInputData {
+        public ConcreteloginLoginInputData(String identification, String password) {
+            super(identification, password);
+        }
+    }
 
     @BeforeEach
     public void setUp() {
         userDataAccessObject = new InMemoryLoginDataAccess();
         presenter = new SimplePresenter();
-        loginInteractor = new LoginInteractor(userDataAccessObject, presenter);
+        loginInteractor = new ConcreteloginInteractor(userDataAccessObject, presenter) {
+            @Override
+            public void execute(LoginInputData inputData) {
+
+            }
+        };
     }
 
     @Test
     public void testUserNotFound() {
         String password = "password123";
         String userId = "nonexistentUser";
-        LoginInputData inputData = new LoginInputData(password, userId);
+        LoginInputData inputData = new ConcreteloginLoginInputData(password, userId);
 
         loginInteractor.execute(inputData);
 
@@ -69,7 +90,7 @@ public class LoginInteractorTest {
     public void testEmptyPassword() {
         String password = "";
         String userId = "existentUser";
-        LoginInputData inputData = new LoginInputData(password, userId);
+        LoginInputData inputData = new ConcreteloginLoginInputData(password, userId);
 
         loginInteractor.execute(inputData);
 
@@ -81,7 +102,7 @@ public class LoginInteractorTest {
     public void testEmptyIdentification() {
         String password = "password123";
         String userId = "";
-        LoginInputData inputData = new LoginInputData(password, userId);
+        LoginInputData inputData = new ConcreteloginLoginInputData(password, userId);
 
         loginInteractor.execute(inputData);
 
@@ -93,7 +114,7 @@ public class LoginInteractorTest {
     public void testEmptyPasswordAndIdentification() {
         String password = "";
         String userId = "";
-        LoginInputData inputData = new LoginInputData(password, userId);
+        LoginInputData inputData = new ConcreteloginLoginInputData(password, userId);
 
         loginInteractor.execute(inputData);
 
@@ -108,7 +129,7 @@ public class LoginInteractorTest {
         String userId = "existentUser";
         UserAccount user = new UserAccount(username, password, userId);
         ((InMemoryLoginDataAccess) userDataAccessObject).addUser(user);
-        LoginInputData inputData = new LoginInputData("wrongPassword", userId);
+        LoginInputData inputData = new ConcreteloginLoginInputData("wrongPassword", userId);
 
         loginInteractor.execute(inputData);
 
@@ -123,7 +144,7 @@ public class LoginInteractorTest {
         String userId = "existentUser";
         UserAccount user = new UserAccount(username, password, userId);
         ((InMemoryLoginDataAccess) userDataAccessObject).addUser(user);
-        LoginInputData inputData = new LoginInputData(password, userId);
+        LoginInputData inputData = new ConcreteloginLoginInputData(password, userId);
 
         ((InMemoryLoginDataAccess) userDataAccessObject).setLoginSuccess(false);
 
@@ -161,10 +182,15 @@ public class LoginInteractorTest {
         }
     }
 
-    private static class SimplePresenter implements LoginOutputBoundary {
+    static class SimplePresenter implements LoginOutputBoundary {
         private String message;
         private LoginOutputData data;
         private boolean success;
+
+        @Override
+        public void prepareSuccessView(Object user) {
+
+        }
 
         @Override
         public void prepareFailView(String message) {
@@ -172,11 +198,6 @@ public class LoginInteractorTest {
             this.success = false;
         }
 
-        @Override
-        public void prepareSuccessView(LoginOutputData data) {
-            this.data = data;
-            this.success = true;
-        }
 
         public String getMessage() {
             return message;
