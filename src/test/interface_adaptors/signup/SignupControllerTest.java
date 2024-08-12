66eebd646def5962012a0af05e66938eaef098e3
Rita Wang <ritaaawzy@gmail.com>
2024-08-12 07:37:05 -0400

    Update tests

--- src/test/interface_adaptors/signup/SignupControllerTest.java
+++ src/test/interface_adaptors/signup/SignupControllerTest.java
@@ -1,46 +1,32 @@
-//package interface_adaptors;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import use_case.signup.shared_account.SharedAccountSignupInputData;
-//import use_case.signup.shared_account.SharedAccountSignupInteractor;
-//import use_case.signup.SignupInputData;
-//import use_case.signup.SignupInteractor;
-//
-//
-//class SignupControllerTest {
-//    private SignupInteractor signupInteractor;
-//    private SharedAccountSignupInteractor sharedAccountSignupInteractor;
-//    private SignupController signupController;
-//
-//    @BeforeEach
-//    void setUp() {
-//        signupInteractor = Mockito.mock(SignupInteractor.class);
-//        sharedAccountSignupInteractor = Mockito.mock(SharedAccountSignupInteractor.class);
-//        signupController = new SignupController(signupInteractor, sharedAccountSignupInteractor);
-//    }
-//
-//    @Test
-//    void testExecuteWithSharedAccount() {
-//        String username = "testuser";
-//        String password = "testpassword";
-//        String identification = "testid";
-//        String sharedAccountId = "testsharedaccountid";
-//
-//        signupController.execute(username, password, identification, sharedAccountId);
-//
-//        verify(sharedAccountSignupInteractor).execute(any(SharedAccountSignupInputData.class));
-//    }
-//
-//    @Test
-//    void testExecuteWithoutSharedAccount() {
-//        String username = "testuser";
-//        String password = "testpassword";
-//        String identification = "testid";
-//        String sharedAccountId = null;
-//
-//        signupController.execute(username, password, identification, sharedAccountId);
-//
-//        verify(signupInteractor).execute(any(SignupInputData.class));
-//    }
-//}
+package interface_adaptors.signup;
+
+import interface_adaptors.signup.user_account.UserAccountSignupController;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import use_case.signup.user_account.UserAccountSignupInputData;
+import use_case.signup.user_account.UserAccountSignupInteractor;
+
+import static org.mockito.Mockito.*;
+
+class SignupControllerTest {
+    private UserAccountSignupInteractor mockSignupInteractor;
+    private UserAccountSignupController controller;
+
+    @BeforeEach
+    void setUp() {
+        mockSignupInteractor = mock(UserAccountSignupInteractor.class);
+        controller = new UserAccountSignupController(mockSignupInteractor);
+    }
+
+    @Test
+    void testExecute() {
+        String username = "testuser";
+        String password = "testpassword";
+        String identification = "testid";
+
+        controller.execute(username, password, identification);
+
+        UserAccountSignupInputData expectedInputData = new UserAccountSignupInputData(username, password, identification);
+        verify(mockSignupInteractor, times(1)).execute(expectedInputData);
+    }
+}
